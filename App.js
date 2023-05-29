655e868ee44ea697ece2ae9d86e6e70a26e99867
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-05-30 08:52:02 +1300

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,21 @@
 
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Event Planner</h1>
+      </header>
+      <main>
+        {/* content here */}
+      </main>
+      <footer>
+        <p>&copy; 2023 Event Planner. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
