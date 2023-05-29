0d51a142eac8e9f6fced69c2d6db60f1991803fd
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-05-30 08:57:01 +1300

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to the Event Planner!</h2>
+      {/* content here */}
+    </div>
+  );
+}
+
+export default Home;
 
