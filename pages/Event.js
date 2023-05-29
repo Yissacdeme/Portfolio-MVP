ef9d192f219ba58efc0c2d2557e5a6c5eb9924d5
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-05-30 08:58:17 +1300

    Update Event.js

--- pages/Event.js
+++ pages/Event.js
@@ -1,1 +1,13 @@
+import React from 'react';
+
+function Event() {
+  return (
+    <div>
+      <h2>Welcome to the Event Planner!</h2>
+      {/* content here */}
+    </div>
+  );
+}
+
+export default Event;
 
