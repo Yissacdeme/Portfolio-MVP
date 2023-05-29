4f0286bd46ac87cc085c1eb7c861e2c22c4c195d
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-05-30 09:07:41 +1300

    Update Event.js

--- pages/Event.js
+++ pages/Event.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import React, { useState } from 'react';
+
+const [eventName, setEventName] = useState('');
+const [eventDate, setEventDate] = useState('');
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setEventName(e.target.elements.eventName.value);
+  setEventDate(e.target.elements.eventDate.value);
+};
 
 function Event() {
   return (
+    return (
+  <div>
+    <h2>Create Event</h2>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Event Name:
+        <input type="text" name="eventName" />
+      </label>
+      <label>
+        Event Date:
+        <input type="date" name="eventDate" />
+      </label>
+      <button type="submit">Create</button>
+    </form>
+    <p>Event Name: {eventName}</p>
+    <p>Event Date: {eventDate}</p>
+  </div>
+);
+
     <div>
       <h2>Welcome to the Event Planner!</h2>
       {/* content here */}
