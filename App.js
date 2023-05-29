58b8e49f471433ac3a1bf6da74723bc0b83c008a
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-05-30 09:00:57 +1300

    Update App.js

--- App.js
+++ App.js
@@ -6,6 +6,29 @@ import './App.css';
 
 function App() {
   return (
+    import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Event Planner</h1>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/event/:id" component={Event} />
+          </Switch>
+        </main>
+        <footer>
+          <p>&copy; 2023 Event Planner. All rights reserved.</p>
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
     <div className="App">
       <header>
         <h1>Event Planner</h1>
