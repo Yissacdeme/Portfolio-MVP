4fcf756170fcdbf0a1be2b03a219c557c2db63db
Yissacdeme <111168633+Yissacdeme@users.noreply.github.com>
2023-06-09 08:09:55 +1300

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -11,3 +11,25 @@ function Home() {
 
 export default Home;
 
+function filterEvents(events, filters) {
+  // Initialize an empty array to store filtered events
+  const filteredEvents = [];
+
+  // Iterate through each event
+  for (let i = 0; i < events.length; i++) {
+    const event = events[i];
+
+    // Check if the event matches the filter criteria
+    if (
+      event.location === filters.location &&
+      event.date === filters.date &&
+      event.category === filters.category
+    ) {
+      // Add the event to the filtered events array
+      filteredEvents.push(event);
+    }
+  }
+
+  // Return the filtered events
+  return filteredEvents;
+}
